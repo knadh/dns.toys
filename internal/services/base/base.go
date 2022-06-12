876ffc6729bd1a6b4d3237a7632bdc79af2aa923
Varun <varun.la@gmail.com>
2022-06-12 15:25:57 +0530

    Handle strings in lowercase for consistency

--- internal/services/base/base.go
+++ internal/services/base/base.go
@@ -15,19 +15,19 @@ func New() *Base {
 	return &Base{}
 }
 
-var numQueryFormat = regexp.MustCompile("([0-9A-F\\.]+)([A-Z]{3})\\-([A-Z]{3})")
+var numQueryFormat = regexp.MustCompile("([0-9a-f\\.]+)([a-z]{3})\\-([a-z]{3})")
 
 var baseStrToNum = map[string]int{
-	"HEX": 16,
-	"DEC": 10,
-	"OCT": 8,
-	"BIN": 2,
+	"hex": 16,
+	"dec": 10,
+	"oct": 8,
+	"bin": 2,
 }
 
 // Query converts a number from one base to another base format
 func (n *Base) Query(q string) ([]string, error) {
 
-	q = strings.ToUpper(q)
+	q = strings.ToLower(q)
 
 	reg := numQueryFormat.FindStringSubmatch(q)
 	if len(reg) != 4 {
@@ -49,7 +49,7 @@ func (n *Base) Query(q string) ([]string, error) {
 		return nil, errors.New("invalid number.")
 	}
 
-	res := strings.ToUpper(strconv.FormatInt(num, toBase))
+	res := strconv.FormatInt(num, toBase)
 
 	r := fmt.Sprintf("%s 1 TXT \"%s %s = %s %s\"", q, reg[1], reg[2], res, reg[3])
 	return []string{r}, nil
