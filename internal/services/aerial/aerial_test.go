e7faed4901164aa99973162bf7266d617783c835
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2022-11-07 22:59:07 +0530

    check out of bounds lat lng in test

--- internal/services/aerial/aerial_test.go
+++ internal/services/aerial/aerial_test.go
@@ -9,21 +9,73 @@ var tests = []struct {
 	l1 aerial.Location
 	l2 aerial.Location
 	d float64
+	errorString string
 }{
 	{
 		aerial.Location{Lat: 30.2458, Lng: 75.8421}, // Sangrur
 		aerial.Location{Lat: 30.2001, Lng: 75.6755}, // Longowal
 		16.793459061041027,
+		"",
 	},
 	{
 		aerial.Location{Lat: 12.9352, Lng: 77.6245},  // Kormangala
 		aerial.Location{Lat: 12.9698, Lng: 77.7500}, // Whitefield
 		14.132940521067107,
+		"",
 	},
 	{
 		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
 		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
 		1750.0305628709923,
+		"",
+	},
+	{
+		aerial.Location{Lat: -120.9716, Lng: 77.5946}, // Wrong Lat
+		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
+		0,
+		"lat out of bounds",
+	},
+	{
+		aerial.Location{Lat: 120.9716, Lng: 77.5946}, // Wrong Lat
+		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
+		0,
+		"lat out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: 277.5946}, // Wrong Lng
+		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
+		0,
+		"lng out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: -277.5946}, // Wrong Lng
+		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
+		0,
+		"lng out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
+		aerial.Location{Lat: 128.7041, Lng: 77.1025}, // Wrong Lat
+		0,
+		"lat out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
+		aerial.Location{Lat: -128.7041, Lng: 77.1025}, // Wrong Lat
+		0,
+		"lat out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
+		aerial.Location{Lat: 28.7041, Lng: -187.1025}, // Wrong Lng
+		0,
+		"lng out of bounds",
+	},
+	{
+		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
+		aerial.Location{Lat: 28.7041, Lng: 187.1025}, // Wrong Lng
+		0,
+		"lng out of bounds",
 	},
 }
 
@@ -31,7 +83,10 @@ func TestCalculateAerialDistance(t *testing.T) {
 	for _, input := range tests {
 		d, err := aerial.CalculateAerialDistance(input.l1, input.l2)
 		if err != nil {
-			t.Errorf("fail: %v %v -> %v got error: %v", input.l1, input.l2, input.d, err)
+			if err.Error() != input.errorString {
+				t.Errorf("fail: %v %v -> %v got error: %v", input.l1, input.l2, input.errorString, err)
+			}
+			continue
 		}
 
 		if input.d != d {
