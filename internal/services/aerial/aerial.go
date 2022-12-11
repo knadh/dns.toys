bbe9c88ea8d8eb6cd455c5d53c7bf5466c2b2049
Kailash Nadh <kailash@nadh.in>
2022-12-11 12:09:51 +0530

    Apply cosmetic fixes.
    - Refactor naming.
    - Reorder and group variable declarations.
    - Refactor style.

--- internal/services/aerial/aerial.go
+++ internal/services/aerial/aerial.go
@@ -11,52 +11,58 @@ import (
 type Aerial struct{}
 
 type Location struct {
-	Lat float64
-	Lng float64
+	Lat  float64
+	Long float64
 }
+
+const (
+	delimiter = ","
+	separator = "/"
+)
+
+var (
+	validPointRegex = "(-?\\d+.\\d+)"
+	latLongPair     = validPointRegex + delimiter + validPointRegex
+	reParse         = regexp.MustCompile("A" + latLongPair + separator + latLongPair)
+)
 
 // New returns a new instance of Aerial.
 func New() *Aerial {
 	return &Aerial{}
 }
 
-var validPointRegex = "(-?\\d+.\\d+)"
-var delimiter = ","
-var separator = "/"
-var latlngpair = validPointRegex + delimiter + validPointRegex
-
-var reParse = regexp.MustCompile("A" + latlngpair + separator + latlngpair)
-
-// Query returns the aerial distance in KMs between lat lng pair
+// Query returns the aerial distance in KMs between lat long pair.
 func (a *Aerial) Query(q string) ([]string, error) {
-	regexGroups := reParse.FindStringSubmatch(q)
+	parts := reParse.FindStringSubmatch(q)
 
-	if len(regexGroups) != 5 {
-		return nil, errors.New("invalid lat lng format")
+	if len(parts) != 5 {
+		return nil, errors.New("invalid lat long format")
 	}
 
-	res := regexGroups[1:]
-	cord := make([]float64, 0, len(res))
+	var (
+		res   = parts[1:]
+		coord = make([]float64, 0, len(res))
+	)
 	for _, p := range res {
-		// iterate overy every point to convert into float
+		// Iterate overy every point to convert into float.
 		f, err := strconv.ParseFloat(p, 64)
 		if err != nil {
-			return nil, fmt.Errorf("invalid point %s; Error: %w", p, err)
+			return nil, fmt.Errorf("invalid point %s: %w", p, err)
 		}
-		cord = append(cord, f)
+		coord = append(coord, f)
 	}
 
-	l1 := Location{Lat: cord[0], Lng: cord[1]}
-	l2 := Location{Lat: cord[2], Lng: cord[3]}
+	l1 := Location{Lat: coord[0], Long: coord[1]}
+	l2 := Location{Lat: coord[2], Long: coord[3]}
 
 	d, e := CalculateAerialDistance(l1, l2)
 	if e != nil {
 		return nil, e
 	}
 
-	result := "aerial distance = " + strconv.FormatFloat(d, 'f', 2, 64) + " KMs"
-
+	result := "aerial distance = " + strconv.FormatFloat(d, 'f', 2, 64) + " KM"
 	r := fmt.Sprintf(`%s 1 TXT "%s"`, q, result)
+
 	return []string{r}, nil
 }
 
@@ -67,17 +73,16 @@ func (n *Aerial) Dump() ([]byte, error) {
 
 // calculates aerial distance in KMs
 func CalculateAerialDistance(l1, l2 Location) (float64, error) {
-	
 	e1 := validateLocation(l1)
 	e2 := validateLocation(l2)
 
 	if e1 != nil || e2 != nil {
-		errString := "";
-		if (e1 != nil) {
-			errString += e1.Error();
+		errString := ""
+		if e1 != nil {
+			errString += e1.Error()
 		}
-		if (e2 != nil) {
-			if (e1 != nil) {
+		if e2 != nil {
+			if e1 != nil {
 				errString += "; "
 			}
 			errString += e2.Error()
@@ -86,15 +91,11 @@ func CalculateAerialDistance(l1, l2 Location) (float64, error) {
 		return 0, errors.New(errString)
 	}
 
-	lat1 := l1.Lat
-	lng1 := l1.Lng
-	lat2 := l2.Lat
-	lng2 := l2.Lng
-
-	radlat1 := float64(math.Pi * lat1 / 180)
-	radlat2 := float64(math.Pi * lat2 / 180)
-
-	radtheta := float64(math.Pi * float64(lng1-lng2) / 180)
+	var (
+		radlat1  = float64(math.Pi * l1.Lat / 180)
+		radlat2  = float64(math.Pi * l2.Lat / 180)
+		radtheta = float64(math.Pi * float64(l1.Long-l2.Long) / 180)
+	)
 
 	d := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
 	if d > 1 {
@@ -109,8 +110,7 @@ func CalculateAerialDistance(l1, l2 Location) (float64, error) {
 }
 
 func isValidPoint(point, maxVal float64) bool {
-	absoluteVal := math.Abs(point)
-	return absoluteVal <= maxVal
+	return math.Abs(point) <= maxVal
 }
 
 func validateLocation(l Location) error {
@@ -121,15 +121,15 @@ func validateLocation(l Location) error {
 		errString += strconv.FormatFloat(l.Lat, 'f', -1, 64) + " lat out of bounds"
 	}
 
-	isLngValid := isValidPoint(l.Lng, 180)
-	if !isLngValid {
-		if (!isLatValid) {
+	isLongValid := isValidPoint(l.Long, 180)
+	if !isLongValid {
+		if !isLatValid {
 			errString += " "
 		}
-		errString += strconv.FormatFloat(l.Lng, 'f', -1, 64) + " lng out of bounds"
+		errString += strconv.FormatFloat(l.Long, 'f', -1, 64) + " long out of bounds"
 	}
 
-	if (errString != "") {
+	if errString != "" {
 		return errors.New(errString)
 	}
 
--- internal/services/aerial/aerial_test.go
+++ internal/services/aerial/aerial_test.go
@@ -14,80 +14,80 @@ var tests = []struct {
 	e           error
 }{
 	{
-		aerial.Location{Lat: 30.2458, Lng: 75.8421}, // Sangrur
-		aerial.Location{Lat: 30.2001, Lng: 75.6755}, // Longowal
+		aerial.Location{Lat: 30.2458, Long: 75.8421}, // Sangrur
+		aerial.Location{Lat: 30.2001, Long: 75.6755}, // Longowal
 		16.793459061041027,
 		nil,
 	},
 	{
-		aerial.Location{Lat: 12.9352, Lng: 77.6245}, // Kormangala
-		aerial.Location{Lat: 12.9698, Lng: 77.7500}, // Whitefield
+		aerial.Location{Lat: 12.9352, Long: 77.6245}, // Kormangala
+		aerial.Location{Lat: 12.9698, Long: 77.7500}, // Whitefield
 		14.132940521067107,
 		nil,
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 77.5946}, // Bengaluru
-		aerial.Location{Lat: 28.7041, Lng: 77.1025}, // New Delhi
+		aerial.Location{Lat: 12.9716, Long: 77.5946}, // Bengaluru
+		aerial.Location{Lat: 28.7041, Long: 77.1025}, // New Delhi
 		1750.0305628709923,
 		nil,
 	},
 	{
-		aerial.Location{Lat: -120.9716, Lng: 77.5946}, // Wrong Lat
-		aerial.Location{Lat: 28.7041, Lng: 77.1025},   // New Delhi
+		aerial.Location{Lat: -120.9716, Long: 77.5946}, // Wrong Lat
+		aerial.Location{Lat: 28.7041, Long: 77.1025},   // New Delhi
 		0,
 		errors.New("-120.9716 lat out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 120.9716, Lng: 77.5946}, // Wrong Lat
-		aerial.Location{Lat: 28.7041, Lng: 77.1025},  // New Delhi
+		aerial.Location{Lat: 120.9716, Long: 77.5946}, // Wrong Lat
+		aerial.Location{Lat: 28.7041, Long: 77.1025},  // New Delhi
 		0,
 		errors.New("120.9716 lat out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 277.5946}, // Wrong Lng
-		aerial.Location{Lat: 28.7041, Lng: 77.1025},  // New Delhi
+		aerial.Location{Lat: 12.9716, Long: 277.5946}, // Wrong Lng
+		aerial.Location{Lat: 28.7041, Long: 77.1025},  // New Delhi
 		0,
 		errors.New("277.5946 lng out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: -277.5946}, // Wrong Lng
-		aerial.Location{Lat: 28.7041, Lng: 77.1025},   // New Delhi
+		aerial.Location{Lat: 12.9716, Long: -277.5946}, // Wrong Lng
+		aerial.Location{Lat: 28.7041, Long: 77.1025},   // New Delhi
 		0,
 		errors.New("-277.5946 lng out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 77.5946},  // Bengaluru
-		aerial.Location{Lat: 128.7041, Lng: 77.1025}, // Wrong Lat
+		aerial.Location{Lat: 12.9716, Long: 77.5946},  // Bengaluru
+		aerial.Location{Lat: 128.7041, Long: 77.1025}, // Wrong Lat
 		0,
 		errors.New("128.7041 lat out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 77.5946},   // Bengaluru
-		aerial.Location{Lat: -128.7041, Lng: 77.1025}, // Wrong Lat
+		aerial.Location{Lat: 12.9716, Long: 77.5946},   // Bengaluru
+		aerial.Location{Lat: -128.7041, Long: 77.1025}, // Wrong Lat
 		0,
 		errors.New("-128.7041 lat out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 77.5946},   // Bengaluru
-		aerial.Location{Lat: 28.7041, Lng: -187.1025}, // Wrong Lng
+		aerial.Location{Lat: 12.9716, Long: 77.5946},   // Bengaluru
+		aerial.Location{Lat: 28.7041, Long: -187.1025}, // Wrong Lng
 		0,
 		errors.New("-187.1025 lng out of bounds"),
 	},
 	{
-		aerial.Location{Lat: 12.9716, Lng: 77.5946},  // Bengaluru
-		aerial.Location{Lat: 28.7041, Lng: 187.1025}, // Wrong Lng
+		aerial.Location{Lat: 12.9716, Long: 77.5946},  // Bengaluru
+		aerial.Location{Lat: 28.7041, Long: 187.1025}, // Wrong Lng
 		0,
 		errors.New("187.1025 lng out of bounds"),
 	},
 	{
-		aerial.Location{Lat: -120.9716, Lng: 77.5946},  // Wrong Lat
-		aerial.Location{Lat: 28.7041, Lng: 187.1025}, // Wrong Lng
+		aerial.Location{Lat: -120.9716, Long: 77.5946},  // Wrong Lat
+		aerial.Location{Lat: 28.7041, Long: 187.1025}, // Wrong Lng
 		0,
 		errors.New("-120.9716 lat out of bounds; 187.1025 lng out of bounds"),
 	},
 	{
-		aerial.Location{Lat: -120.9716, Lng: 277.5946},  // Wrong Lat Lng
-		aerial.Location{Lat: 28.7041, Lng: 187.1025}, // Wrong Lng
+		aerial.Location{Lat: -120.9716, Long: 277.5946},  // Wrong Lat Lng
+		aerial.Location{Lat: 28.7041, Long: 187.1025}, // Wrong Lng
 		0,
 		errors.New("-120.9716 lat out of bounds 277.5946 lng out of bounds; 187.1025 lng out of bounds"),
 	},
