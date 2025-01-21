fb1d2173c79b13309f8df08b0aaa31e93c211662
Gopalakrishnan Venkatesh <Gopalakrishnan.Venkatesh@oracle.com>
2025-01-21 15:39:53 +0530

    move json contents and then delete folder

--- scripts/update-ifsc.sh
+++ scripts/update-ifsc.sh
@@ -9,4 +9,5 @@ rm -r by-bank
 curl -L -s $(curl -s https://api.github.com/repos/razorpay/ifsc/releases/latest | grep "browser_download_url" | grep "by-bank.tar.gz" | sed -E 's/.*"([^"]+)".*/\1/') -o by-bank.tar.gz
 tar -xzf by-bank.tar.gz
 rm by-bank.tar.gz
+mv by-bank/*.json .
 rm -r by-bank
